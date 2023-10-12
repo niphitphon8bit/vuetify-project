bcdf1ef10a2f8b2785188f1de20886a0ae60650e
niphitphon8bit <niphitphon_nt@hotmail.com>
2023-10-12 21:43:58 +0700

    add  - mock data to project update  - PaperResearchPage with mockData

--- src/components/PaperResearchPage.vue
+++ src/components/PaperResearchPage.vue
@@ -1,6 +1,38 @@
 <template>
-  <h1>test Paper research Page</h1>
+  <v-responsive>
+    <v-container>
+      <v-card>
+        <v-card-title>
+          Paper Research
+          <v-spacer></v-spacer>
+          <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+          ></v-text-field>
+        </v-card-title>
+        <v-data-table
+        :headers="headers"
+        :items="papers"
+        :search="search"
+        ></v-data-table>
+      </v-card>
+    </v-container>
+  </v-responsive>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import { mockPapers } from "@/mockPapers.js";
+
+const search = ref('');
+const papers = mockPapers;
+
+const headers = [
+  { text: 'ID', value: 'id', align: 'start', sortable: true },
+  { text: 'Filename', value: 'filename' },
+  { text: 'Upload Date', value: 'upload_date' },
+];
 </script>
